2e07f475a639b2fc9af5306497ae925929cc8d8f
bottkars <karsten.bott@dell.com>
2024-04-10 11:20:54 +0200

    T-Guard Network rework

--- terraforming-azure/modules/ddve/ddve.tf
+++ terraforming-azure/modules/ddve/ddve.tf
@@ -149,6 +149,7 @@ resource "azurerm_storage_account" "ddve_atos" {
   account_replication_type = "LRS"
   network_rules {
     default_action             = "Deny"
+    ip_rules                   = ["127.0.0.1"]
     virtual_network_subnet_ids = [var.subnet_id]
   }
   tags = {

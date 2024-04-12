85fb896a9fbf64ea081b0e63408fc846de3f88dc
bottkars <karsten.bott@bottnet.de>
2024-04-12 14:27:35 +0200

    depend on module common rg

--- terraforming-azure/main.tf
+++ terraforming-azure/main.tf
@@ -114,6 +114,7 @@ module "ddve" {
   source                            = "./modules/ddve"
   count                             = var.ddve_count > 0 ? var.ddve_count : 0
   ddve_instance                     = count.index + 1
+  depends_on                        = [module.networks, module.common_rg]
   ddve_type                         = var.ddvelist[count.index].ddve_type
   ddve_version                      = var.ddvelist[count.index].ddve_version
   ddve_meta_disks                   = var.ddvelist[count.index].ddve_meta_disks
@@ -125,7 +126,7 @@ module "ddve" {
   subnet_id                         = var.create_networks ? module.networks[0].infrastructure_subnet_id : var.networks_infrastructure_subnet_id
   public_ip                         = var.ddve_public_ip
   wan_ip                            = var.wan_ip
-  ddve_resource_group_name          = var.create_common_rg ? var.common_resource_group_name : var.create_networks ? module.networks[0].resource_group_name: var.resource_group_name
+  ddve_resource_group_name          = var.create_common_rg ? var.common_resource_group_name : var.create_networks ? module.networks[0].resource_group_name : var.resource_group_name
   ddve_networks_resource_group_name = var.create_networks ? module.networks[0].resource_group_name : var.networks_infrastructure_resource_group_name
 }
 
@@ -133,7 +134,7 @@ module "ppdm" {
   count                             = var.ppdm_count > 0 ? var.ppdm_count : 0
   source                            = "./modules/ppdm"
   ppdm_instance                     = count.index + 1
-  depends_on                        = [module.networks,module.common_rg]
+  depends_on                        = [module.networks, module.common_rg]
   ppdm_version                      = var.ppdm_version
   ppdm_initial_password             = var.ppdm_initial_password
   environment                       = var.environment
@@ -142,7 +143,7 @@ module "ppdm" {
   subnet_id                         = var.create_networks ? module.networks[0].infrastructure_subnet_id : var.networks_infrastructure_subnet_id
   public_ip                         = var.ppdm_public_ip
   ppdm_name                         = var.ppdm_name
-  ppdm_resource_group_name          = var.create_common_rg ? var.common_resource_group_name : var.create_networks ? module.networks[0].resource_group_name: var.resource_group_name
+  ppdm_resource_group_name          = var.create_common_rg ? var.common_resource_group_name : var.create_networks ? module.networks[0].resource_group_name : var.resource_group_name
   ppdm_networks_resource_group_name = var.create_networks ? module.networks[0].resource_group_name : var.networks_infrastructure_resource_group_name
 }
 

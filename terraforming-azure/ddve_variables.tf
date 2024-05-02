0cc2c95d585f576f97646845ea12e2511ccf5c4a
bottkars <karsten.bott@bottnet.de>
2024-05-02 11:09:48 +0200

    latest

--- terraforming-azure/ddve_variables.tf
+++ terraforming-azure/ddve_variables.tf
@@ -40,55 +40,24 @@ variable "ddve_public_ip" {
 
 variable "ddvelist" {
   type = map(object({
-      ddve_name = string
-      ddve_meta_disks = list(string)
-      ddve_type       = string
-      ddve_version    = string
-}))
+    ddve_name       = string
+    ddve_meta_disks = list(string)
+    ddve_type       = string
+    ddve_version    = string
+  }))
 
   default = {
-   firstdd = {
-      ddve_name = "ddve1",
-      ddve_meta_disks = [1000, 1000],
-      ddve_type       = "16 TB DDVE",
-      ddve_version    = "7.13.020"
-   }
+    firstdd = {
+      ddve_name       = "ddve1"
+      ddve_meta_disks = [1000, 1000]
+      ddve_type       = "16 TB DDVE"
+      ddve_version    = "8.0.010.MSDN"
+    }
   }
-}
-variable "ddve_name" {
   validation {
     condition     = length(var.ddve_name) >= 1 && length(var.ddve_name) <= 15
     error_message = "The Name length oh the ddve  must not exceed 15 chars"
   }
-}
-variable "ddve_version" {
-  type        = string
-  default     = "8.0.010.MSDN"
-  description = "DDVE Version, can be: '7.7.525', '7.7.530', '7.10.115', '7.10.120', '7.13.020', '8.0.010', '7.10.1015.MSDN', '7.10.120.MSDN', '7.7.5020.MSDN', '7.13.0020.MSDN', '8.0.010.MSDN' "
-  validation {
-    condition = anytrue([
-      var.ddve_version == "7.7.525",
-      var.ddve_version == "7.7.530",
-      var.ddve_version == "7.10.115",
-      var.ddve_version == "7.10.120",
-      var.ddve_version == "7.13.020",
-      var.ddve_version == "8.0.010",
-      var.ddve_version == "7.10.1015.MSDN",
-      var.ddve_version == "7.10.120.MSDN",
-      var.ddve_version == "7.7.5020.MSDN",
-      var.ddve_version == "7.7.530.MSDN",      
-      var.ddve_version == "7.13.0020.MSDN",
-      var.ddve_version == "8.0.010.MSDN"
-    ])
-    error_message = "Must be a valid DDVE Version, can be:  '7.7.525', '7.7.530', '7.10.115', '7.10.120', '7.13.020', '8.0.010', '7.10.1015.MSDN', '7.10.120.MSDN', '7.7.5020.MSDN', '7.13.0020.MSDN', '8.0.010.MSDN' ."
-  }
-}
-
-
-variable "ddve_type" {
-  type        = string
-  default     = "16 TB DDVE"
-  description = "DDVE Type, can be: '16 TB DDVE', '32 TB DDVE', '96 TB DDVE', '256 TB DDVE','16 TB DDVE PERF', '32 TB DDVE PERF', '96 TB DDVE PERF', '256 TB DDVE PERF'"
   validation {
     condition = anytrue([
       var.ddve_type == "16 TB DDVE",
@@ -102,5 +71,23 @@ variable "ddve_type" {
     ])
     error_message = "Must be a valid DDVE Type, can be: '16 TB DDVE', '32 TB DDVE', '96 TB DDVE', '256 TB DDVE'."
   }
+  validation {
+    condition = anytrue([
+      var.ddve_version == "7.7.525",
+      var.ddve_version == "7.7.530",
+      var.ddve_version == "7.10.115",
+      var.ddve_version == "7.10.120",
+      var.ddve_version == "7.13.020",
+      var.ddve_version == "8.0.010",
+      var.ddve_version == "7.10.1015.MSDN",
+      var.ddve_version == "7.10.120.MSDN",
+      var.ddve_version == "7.7.5020.MSDN",
+      var.ddve_version == "7.7.530.MSDN",
+      var.ddve_version == "7.13.0020.MSDN",
+      var.ddve_version == "8.0.010.MSDN"
+    ])
+    error_message = "Must be a valid DDVE Version, can be:  '7.7.525', '7.7.530', '7.10.115', '7.10.120', '7.13.020', '8.0.010', '7.10.1015.MSDN', '7.10.120.MSDN', '7.7.5020.MSDN', '7.13.0020.MSDN', '8.0.010.MSDN' ."
+  }
 }
+
 

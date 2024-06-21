e61075d96e5fb8e04d8be84e7c49252cd84b3838
bottkars <karsten.bott@bottnet.de>
2024-06-21 14:06:21 +0200

    windows#

--- terraforming-gcp/windows_output.tf
+++ terraforming-gcp/windows_output.tf
@@ -1,22 +1,22 @@
-output "ubuntu_private_ip" {
-  value       = var.ubuntu_count > 0 ? module.ubuntu[0].ubuntu_private_ip : null
+output "windows_private_ip" {
+  value       = var.windows_count > 0 ? module.windows[0].windows_private_ip : null
   description = "The private ip address for the DDVE Instance"
 }
 
-output "ubuntu_instance_id" {
-  value       = var.ubuntu_count > 0 ? module.ubuntu[0].ubuntu_instance_id : null
+output "windows_instance_id" {
+  value       = var.windows_count > 0 ? module.windows[0].windows_instance_id : null
   description = "The instance id (initial password) for the DDVE Instance"
   sensitive   = true
 }
-output "ubuntu_ssh_private_key" {
+output "windows_ssh_private_key" {
   sensitive   = true
-  value       = var.ubuntu_count > 0 ? module.ubuntu[0].ssh_private_key : null
+  value       = var.windows_count > 0 ? module.windows[0].ssh_private_key : null
   description = "The ssh private key for the DDVE Instance"
 }
 
 
-output "ubuntu_ssh_public_key" {
-  value       = var.ubuntu_count > 0 ? module.ubuntu[0].ssh_public_key : null
+output "windows_ssh_public_key" {
+  value       = var.windows_count > 0 ? module.windows[0].ssh_public_key : null
   sensitive   = true
   description = "The ssh public key for the DDVE Instance"
 }

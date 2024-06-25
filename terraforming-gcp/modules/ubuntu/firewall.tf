1a5c9a732c5ecb7103ebb0ba545b31171c8619ee
bottkars <karsten.bott@bottnet.de>
2024-06-25 17:30:59 +0200

    details

--- terraforming-gcp/modules/ubuntu/firewall.tf
+++ terraforming-gcp/modules/ubuntu/firewall.tf
@@ -27,9 +27,14 @@ resource "google_compute_firewall" "ubuntu-egress" {
   allow {
     protocol = "icmp"
   }
+
   allow {
     protocol = "tcp"
-    ports    = ["443"]
+    ports    = ["389", "443", "636", "3009", "5989", "7000", "25", "587", "143", "993", "2702", "111", "2049", "2052", "9443", "9090", "9613", "30095", "14251"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["123", "162", "514"]
   }
   source_tags = var.source_tags
   target_tags = concat(
